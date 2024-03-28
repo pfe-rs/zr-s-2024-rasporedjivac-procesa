338e42ea0cd9259276ffefe6efc58fef1581e3ad
Veliki5382 <kuzmanov03@gmail.com>
2024-03-28 14:43:52 +0100

    #4 Dodata klasa Process

---
+++ src/process.py
@@ -0,0 +1,11 @@
+class Process:
+    def __init__(self, num_of_iterations, sleep_interval, process_size):
+        self.num_of_iterations = num_of_iterations
+        self.sleep_interval = sleep_interval
+        self.process_size = process_size
+
+class CPU:
+    def __init__(self):
+        
+
+        
