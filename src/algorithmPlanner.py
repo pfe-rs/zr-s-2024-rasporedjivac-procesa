e13ca111bd0d99d12c3ea1c51be936ce8e0f0b04
Veliki5382 <kuzmanov03@gmail.com>
2024-03-28 22:23:31 +0100

    #7 Dodata SmallestProcessFirst klasa opet opet opet i njena funkcionalnost.

--- src/algorithmPlanner.py
+++ src/algorithmPlanner.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-import queue
 from queue import PriorityQueue
 from dataclasses import dataclass, field
 
@@ -15,8 +14,24 @@ class AlgorithmPlanner(ABC):
     
 class FirstComeFirstServe(AlgorithmPlanner):
     def __init__(self):
-        self.procesess = queue.Queue()
+        pass
     def get_process(self):
-        return self.procesess.get()
+        pass
     def put_process(self, proc):
         pass
+
+
+@dataclass(order=True)
+class ShortestProcessFirst(AlgorithmPlanner):
+    def __init__(self):
+        self.process_queue = PriorityQueue()
+
+    def get_process(self):
+        if not self.process_queue.empty():
+            return self.process_queue.get()
+        else:
+            return None
+
+    def put_process(self, proc):
+        proc.numOfIterations = field(compare=False)
+        self.process_queue.put(proc)
