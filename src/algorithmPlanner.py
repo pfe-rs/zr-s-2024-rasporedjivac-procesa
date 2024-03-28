89416cd887ced03b6b39c10fbb5bc6161a66e67f
NatasaKovacevicCoconut <kovacevic.natasa.bgd@gmail.com>
2024-03-28 21:13:24 +0100

    #3 gotova abstraktna klasa algpln

--- src/algorithmPlanner.py
+++ src/algorithmPlanner.py
@@ -3,3 +3,15 @@ from abc import ABC, abstractmethod
 class AlgorithmPlanner(ABC):
     def __init__(self):
         pass
+    def get_process(self):
+        pass
+    def put_process(self, proc):
+        pass
+    
+class FirstComeFirstServe(AlgorithmPlanner):
+    def __init__(self):
+        pass
+    def get_process(self):
+        pass
+    def put_process(self, proc):
+        pass
